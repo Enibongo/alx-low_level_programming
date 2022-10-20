d50742538b1660fd42ee3e5e52531e712efe5add
Enibongo <enobongenobong01@gmail.com>
2022-10-19 18:56:09 -0700

    vig

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,7 @@
-int _islower(int c);	
+int _islower(int c);
+int putchar (int character);
+int putchar(int char);
+putchar(character);
+putchar(character_variable);
+int putchar(const char *string);
+int putchar(int character);
